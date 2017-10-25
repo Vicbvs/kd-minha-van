d49a82f73b3f16b561e3d079ef121a0881a973f0
Julio Oliveira <julioflavio.m@gmail.com>
2017-10-25 08:54:40 -0200

    validacao formulario login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
@@ -15,11 +16,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formLogin: FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+
+    let emailReg = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+    
+    this.formLogin = this.formBuilder.group({
+      email: [ '', [ Validators.compose([ Validators.required, Validators.pattern(emailReg) ]) ] ],
+      senha: [ '', [ Validators.required, Validators.minLength(6) ] ],
+    })
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+  fazerLogin() {
+    console.log(this.formLogin.value);
+    
   }
 
 }
