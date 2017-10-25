c2c2e44ab6897af650d53a17d4bba88c3bf514e6
Julio Oliveira <julioflavio.m@gmail.com>
2017-10-24 22:13:31 -0200

    add e configurado firebase e angular fire 2

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBEfak2xFJjxs2faHU3P-1Ag7yKsdQg1-4",
+    authDomain: "kd-minha-van.firebaseapp.com",
+    databaseURL: "https://kd-minha-van.firebaseio.com",
+    projectId: "kd-minha-van",
+    storageBucket: "kd-minha-van.appspot.com",
+    messagingSenderId: "56954423986"
+  };
